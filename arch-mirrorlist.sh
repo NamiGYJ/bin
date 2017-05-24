f90c1798c0f2bdf9358fbeaaaaf5e774ce3ec66d
ngyj <namigyj@users.noreplay.github.com>
2017-05-24 11:08:49 +0200

    first commit

---
+++ arch-mirrorlist.sh
@@ -0,0 +1,5 @@
+curl https://raw.githubusercontent.com/Gen2ly/armrr/master/armrr >> armrr
+chmod +x armrr
+./armrr
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.b
+rankmirrors /etc/pacman.d/mirrorlist.b > /etc/pacman.d/mirrorlist
