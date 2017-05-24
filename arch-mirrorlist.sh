556f1e36436f8f4ee09d976f705bdc8d9fcee072
ngyj <namigyj@users.noreplay.github.com>
2017-05-24 12:09:45 +0200

    >

--- arch-mirrorlist.sh
+++ arch-mirrorlist.sh
@@ -1,5 +1,6 @@
-curl https://raw.githubusercontent.com/Gen2ly/armrr/master/armrr >> armrr
+curl https://raw.githubusercontent.com/Gen2ly/armrr/master/armrr > armrr
 chmod +x armrr
 ./armrr
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.b
 rankmirrors /etc/pacman.d/mirrorlist.b > /etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist
