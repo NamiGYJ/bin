ae90473caa6f6725bb120b1eb8e04f3b1b5afbbd
ngyj <ngyj@lptp>
2017-09-20 11:14:15 +0200

    ~yt-album-disasm.sh : time difference calculation and outpu

--- youtube-album-disassembler.sh
+++ youtube-album-disassembler.sh
@@ -12,31 +12,78 @@ timelist=(
 	"05:00" # end time
 )
 
-i=2
+i=0
+file="file.mp3"
+output="artist"
 
-divtime=$(echo ${timelist[i]} | tr ":" "\n")
+	divtime=$(echo ${timelist[i]} | tr ":" "\n")
 
-j=0
-for wrd in $divtime
-do
-	divtimelist[j]=$wrd
-	j=$[ $j+1 ]
-done
-echo "$j"
+	j=0
+	for wrd in $divtime
+	do
+		divtimelist[j]=$wrd
+		j=$[ $j+1 ]
+	done
+	echo "$j"
 
-echo "${divtimelist[0]}:${divtimelist[1]}:${divtimelist[2]}"
 
-if [ $j -lt 2 ] || [ $j -gt 3 ]; then
-	echo "error in timelist[i]"
-	exit 1
-fi
+	if [ $j -lt 2 ] || [ $j -gt 3 ]; then
+		echo "error in timelist[i]"
+		exit 1
+	fi
 
-if [ $j -eq 2 ]
-then
-	divtimelist[2]=${divtimelist[1]}
-	divtimelist[1]=${divtimelist[0]}
-	divtimelist[0]="00"
-fi
+	if [ $j -eq 2 ]
+	then
+		divtimelist[2]=${divtimelist[1]}
+		divtimelist[1]=${divtimelist[0]}
+		divtimelist[0]="00"
+		echo "added a zero"
+	fi
 
-echo "${divtimelist[0]}:${divtimelist[1]}:${divtimelist[2]}"
+	timelist[i]="${divtimelist[0]}:${divtimelist[1]}:${divtimelist[2]}"
+	echo "  ${timelist[i]}"
+
+	next=$[ $i+1 ]
+	divtime2=$(echo ${timelist[next]} | tr ":" "\n")
+
+	j=0
+	for wrd in $divtime2
+	do
+		divtimelist2[j]=$wrd
+		j=$[ $j+1 ]
+	done
+	if [ $j -lt 2 ] || [ $j -gt 3 ]; then
+		echo "eroor in timelist[i]"
+		exit 1
+	fi
+	if [ $j -eq 2 ]; then
+		divtimelist2[2]=${divtimelist2[1]}
+		divtimelist2[1]=${divtimelist2[0]}
+		divtimelist2[0]="00"
+	fi
+	echo "- ${divtimelist2[0]}:${divtimelist2[1]}:${divtimelist2[2]}"
+
+	sec=$[ ${divtimelist2[2]} - ${divtimelist[2]} ]
+	min=$[ ${divtimelist2[1]} - ${divtimelist[1]} ]
+	hor=$[ ${divtimelist2[0]} - ${divtimelist[0]} ]
+	diff=$[ ($sec) + ($min*60) + ($hor*3600) ]
+	echo "= $diff"
+
+	output="$output - ${namelist[i]}"
+	echo "bin/ffmpeg.exe -ss ${timelist[i]} -t $diff -i $file -vn -acodec aac -vbr 5 -crf 10 $output.aac"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
