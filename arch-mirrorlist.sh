de1a1e7e13fb9cd41b4c9b03988e8da934c29a3f
ngyj <namigyj@inori-kusanagi.home>
2017-10-20 13:55:51 +0000

    ~mirrorslist : delete armrr

--- arch-mirrorlist.sh
+++ arch-mirrorlist.sh
@@ -3,4 +3,5 @@ chmod +x armrr
 ./armrr
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.b
 rankmirrors /etc/pacman.d/mirrorlist.b > /etc/pacman.d/mirrorlist
+rm ./armrr
 cat /etc/pacman.d/mirrorlist
